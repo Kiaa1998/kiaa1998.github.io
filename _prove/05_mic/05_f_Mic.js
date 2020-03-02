697a19d65337d6e5e96edbe276aed92e088f93e1
Chiara <37084821+Kiaa1998@users.noreply.github.com>
2020-03-02 22:16:30 +0100

    Prova_p5_mic
    Mic needs https to access data?

---
+++ _prove/05_mic/05_f_Mic.js
@@ -0,0 +1,28 @@
+// INTERACTION & INTERFACES //
+// PART 1f: Mic //
+
+let mic;
+
+function preload() {
+}
+
+function setup() {
+  createCanvas(500,500);
+  mic = new p5.AudioIn(); // create an Audio input line
+  mic.start(); // starts the audio input
+}
+
+function draw() {
+  background(200);
+
+  var vol = mic.getLevel(); // this should get microphone input level as a value between 0 and 1
+  console.log(vol)
+
+  fill(255,0,0);
+  noStroke();
+  ellipse(250,250,vol*200);
+}
+
+// Input always returns = 0
+// THERE'S AN ISSUE WITH THE LIBRARY MOST LIKELY
+// THEORETICALLY THE CODE WORKS
